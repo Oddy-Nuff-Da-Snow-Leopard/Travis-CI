170bf15d0878b7f5f4603ee2453876ef47d10d9e
Maksim Alekseev <angry.school.boy@mail.ru>
2020-01-09 17:07:37 +0300

    setup

--- CI/src/test/java/by/bstu/unittesting/page/AbstractPage.java
+++ CI/src/test/java/by/bstu/unittesting/page/AbstractPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 
 public abstract class AbstractPage {
 
-    protected static final int SECONDS_TO_LOAD = 30;
+    protected static final int SECONDS_TO_LOAD = 15;
     protected static final int SECONDS_TO_WAIT = 3;
 
     protected WebDriver driver;
